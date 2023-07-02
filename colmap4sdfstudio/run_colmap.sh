a80407f11b244d77e1c90f736f0eda48267bfbfc
Youming Deng <56004925+denghilbert@users.noreply.github.com>
2023-07-02 23:08:23 +0200

    Update run_colmap.sh

--- colmap4sdfstudio/run_colmap.sh
+++ colmap4sdfstudio/run_colmap.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# colmaping
+ns-process-data images --data ~/Desktop/eu_colmap/colmap/images --output-dir ~/Desktop/eu_colmap/
+
+# generate mask
 ns-process-data images --data ~/Desktop/eu_colmap/colmap/images --output-dir ~/Desktop/eu_colmap/ --skip-colmap
